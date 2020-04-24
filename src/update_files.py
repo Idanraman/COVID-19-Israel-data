539bdd9147e1ef784864efae5417a2ff53787000
Idanraman <idanraman@gmail.com>
2020-04-25 00:11:08 +0300

    Update update_files.py

--- src/update_files.py
+++ src/update_files.py
@@ -9,10 +9,10 @@ OUTPUT_DIR = os.path.join(SCRIPT_DIR,'..',"data")
 CITIES_DIR = os.path.join(SCRIPT_DIR,"telegram_files","raw_cities_files")
 
 def main():
-    try:
-        download_telegram.main_runner()
-    except Exception as e:
-        print('Error at %s', 'telegram scraping', exc_info=e)
+    #try:
+    #    download_telegram.main_runner()
+    #except Exception as e:
+    #    print('Error at %s', 'telegram scraping', exc_info=e)
 
     try:
         print(os.listdir(CITIES_DIR))
@@ -21,12 +21,12 @@ def main():
             cities_parser = cp.CitiesFileParser(path, OUTPUT_DIR)
             cities_parser.run()
     except Exception as e:
-        print('Error at %s', 'cities parsing', exc_info=e)
+        print('Error at %s', 'cities parsing')
 
     try:
         imh_website_scraper.main(OUTPUT_DIR)
     except Exception as e:
-        print('Error at %s', 'ihm website scraping', exc_info=e)
+        print('Error at %s', 'ihm website scraping')
 
 
 
